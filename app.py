cc1d02ef03fd22374b710beb288bf429eb7bb513
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-02-21 07:56:37 +0000

    made the conditional of empty name on the template, instead of the backend

--- app.py
+++ app.py
@@ -5,6 +5,6 @@ app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def index():
     if request.method=='POST':
-        name=request.form.get('name','World')
+        name=request.form.get('name')
         return render_template('greet.html',name=name)
     return render_template('index.html')
